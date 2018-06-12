3868e2fcbba66f2284aee060bd0445a466566faf
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-06-12 17:59:17 +0000

    Add function to generate random string

--- express-server.js
+++ express-server.js
@@ -1,10 +1,28 @@
-var express = require("express");
-var app = express();
-var PORT = 8080;
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
-var urlDatabase = {
+const generateRandomString = () => {
+  let outStr = ""
+  while(outStr.length < 6){
+    let randCharCode = Math.floor(Math.random() * 122 + 48)
+    //number
+    if(randCharCode < 58){
+      outStr += String.fromCharCode(randCharCode);
+    //cap and lower letters
+    }else if((randCharCode >= 65 && randCharCode <= 90) || (randCharCode >= 97 && randCharCode <= 122)) {
+      outStr += String.fromCharCode(randCharCode);
+    }
+  }
+  return outStr;
+};
+
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -18,6 +36,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:id", (req, res) => {
 
   let templateVars = {
@@ -29,4 +56,7 @@ app.get("/urls/:id", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+  for(let i=0; i<50; i++){
+    console.log(generateRandomString())
+  }
 });
